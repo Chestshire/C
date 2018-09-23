46a416ec1ee090174eb1b023a2eb49669aaadd59
Chestshire <a250582992@hotmail.com>
2018-09-22 20:41:59 -0400

    add another algorithm to GCD

--- Tools/GCD/findGCD.c
+++ Tools/GCD/findGCD.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 
-int gcd_1(int a, int b)
+// euclidean algorithm
+int gcdEu(int a, int b)
 {
-    return b == 0 ? a : gcd_1(b, a % b);
+    return b == 0 ? a : gcdEu(b, a % b);
 }
 
-int gcd_2(int a, int b)
+// stein algorithm. better when big prime numbers are involved.
+int Min(int a, int b)
 {
+    return a < b ? a : b;
+}
 
-    return 0;
+int abs(int a)
+{
+    return a >= 0 ? a : -a;
+}
+
+int gcdStein(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a % 2 == 0 && b % 2 == 0)
+        return 2 * gcdStein(a >> 1, b >> 1);
+    else if (a % 2 == 0)
+        return gcdStein(a >> 1, b);
+    else if (b % 2 == 0)
+        return gcdStein(a, b >> 1);
+    else
+        return gcdStein(abs(a - b), Min(a, b));
 }
 
 int main(void)
 {
-    int a = 0, b = 0, gcd = 0;
+    int a = 0, b = 0, gcd1 = 0, gcd2 = 0;
     scanf("%d %d", &a, &b);
-    gcd = gcd_1(a, b);
-    //gcd = gcd_2(a, b);
-    printf("GCD: %d", gcd);
+    gcd1 = gcdEu(a, b);
+    gcd2 = gcdStein(a, b);
+    if (gcd1 != gcd2)
+    {
+        printf("someting's wrong");
+    }
+    else
+    {
+        printf("GCD_1 = GCD_2 : %d = %d", gcd1, gcd2);
+    }
     return 0;
 }
